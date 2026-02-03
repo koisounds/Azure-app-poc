dac0198b0edee5a63ce77d070b752e292751fe34
Alex Galotti <n.alexgalotti@gmail.com>
2026-02-03 11:03:34 -0600

    update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,33 @@
-resource "azurerm_resource_group" "app" {
-  name     = var.resource_group_name
-  location = var.location
+########################
+# Existing Resource Group (DO NOT CREATE)
+########################
+data "azurerm_resource_group" "app" {
+  name = var.resource_group_name
 }
 
+########################
+# App Service Plan
+########################
 resource "azurerm_service_plan" "plan" {
   name                = "${var.name_prefix}-plan"
-  resource_group_name = azurerm_resource_group.app.name
-  location            = azurerm_resource_group.app.location
+  location            = data.azurerm_resource_group.app.location
+  resource_group_name = data.azurerm_resource_group.app.name
   os_type             = "Linux"
   sku_name            = var.sku_name
 }
 
+########################
+# Linux Web App
+########################
 resource "azurerm_linux_web_app" "app" {
   name                = "${var.name_prefix}-app"
-  resource_group_name = azurerm_resource_group.app.name
-  location            = azurerm_resource_group.app.location
+  location            = data.azurerm_resource_group.app.location
+  resource_group_name = data.azurerm_resource_group.app.name
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
+    always_on = true
+
     application_stack {
       node_version = "18-lts"
     }
